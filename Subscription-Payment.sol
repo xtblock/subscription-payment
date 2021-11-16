12b94043fcf81a7328325718c6b223d314a68d5f
chorusg <78986667+chorusg@users.noreply.github.com>
2021-11-17 04:37:11 +0700

    Update Subscription-Payment.sol

--- Subscription-Payment.sol
+++ Subscription-Payment.sol
@@ -169,7 +169,8 @@ contract SubscriptionPayment is Ownable {
         
         uint256 currentBalance = paymentToken().balanceOf(address(this));
         
-        paymentToken().safeTransferFrom(address(this), beneficiary(), currentBalance);
+        //paymentToken().safeTransferFrom(address(this), beneficiary(), currentBalance);
+        paymentToken(paymentId).safeTransfer(beneficiary(), currentBalance);
         emit WithdrawTokenForPayment(address(this), beneficiary(), currentBalance);
     }
     
